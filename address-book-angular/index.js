be730d0bc96884cef437619e94e25c23551bbcf4
Sheryl Boughton <sherpbo@gmail.com>
2015-09-02 14:01:21 -0700

    dummy data in case none available

--- address-book-angular/index.js
+++ address-book-angular/index.js
@@ -1,14 +1,3 @@
-// $(document).ready(function(){
-//   $("#submit").on("click", function(){
-//
-//     $("input").on("keyup", function(e){
-//       if(e.which == 13){
-//         contact.editable = false;
-//       }
-//     });
-//   });
-// });
-
 
 angular.module("Contacts", ["angular-md5", "xeditable"])
 .run(function(editableOptions) {
@@ -17,17 +6,21 @@ angular.module("Contacts", ["angular-md5", "xeditable"])
   .controller("ContactsCtrl", function($scope, md5){
     console.log("up to date");
 
-    var contacts = JSON.parse(localStorage.contacts);
-    console.log(contacts);
-    if(!contacts[0]){
+    var allDataStored = Object.keys(localStorage);
+    console.log(allDataStored.indexOf("contacts"));
+
+    if(allDataStored.indexOf("contacts") < 0){
+      $scope.contactsArr = JSON.parse(localStorage.contacts);
+    }else if(allDataStored.indexOf("contacts") >= 0) {
       $scope.contactsArr = [
         {name: "Tania Leonian", email: "tania.dev77@gmail.com", phone: "510-798-3716", editable: false},
         {name: "Lionel Briones", email: "lionelbriones@gmail.com", phone: "", editable: false},
         {name: "Samer Buna", email: "samer.buna@gmail.com", phone: "", editable: false}
       ];
-    } else {
-      $scope.contactsArr = contacts;
+      // debugger;
     }
+
+
     $scope.addContact = function(obj){
       $scope.contactsArr.push(obj);
       console.log($scope.contactsArr);
